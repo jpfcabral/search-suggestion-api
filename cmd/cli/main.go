d68ddc1d7dba5568148e8285edab5c11e2fffccb
João Pedro <jpfcabral@gmail.com>
2024-03-26 17:55:09 -0300

    add cli

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	avltree "github.com/jpfcabral/search-suggestion-api/internal/trees"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	tree := new(avltree.AVLTree)
-	tree.PopulateFromCorpus("assets/br-utf8.txt")
-	tree.DisplayInOrder()
+	rootCmd := &cobra.Command{
+		Use:   "autocom",
+		Short: "An autocomplete system",
+		Run: func(cmd *cobra.Command, args []string) {
+			tree := &avltree.AVLTree{}
+			tree.PopulateFromCorpus("assets/br-utf8.txt")
+
+			if len(args) < 1 {
+				fmt.Println("Provide a prefix")
+				return
+			} else if len(args) > 1 {
+				fmt.Println("Provide just one prefix")
+				return
+			}
+
+			prefix := args[0]
+
+			fmt.Println(tree.WordsWithPrefix(prefix))
+
+		},
+	}
+
+	// Execute the root command
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
